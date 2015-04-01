6ecff6058a21a5a86e487d13d98920e0ad6fa90e
Carl Swanson <cetswanson@gmail.com>
2015-04-01 10:01:18 -0400

    add initialize method to Actor class

--- 2-modeling/imdb_clone.rb
+++ 2-modeling/imdb_clone.rb
@@ -3,4 +3,14 @@
 
 class Actor
 
+  def initialize(first_name, last_name, birth_name, photo_name, photo_url)
+
+    @first_name = first_name
+    @last_name = last_name
+    @birth_name = birth_name
+    @photo_name = photo_name
+    @photo_url = photo_url
+
+  end
+
 end
