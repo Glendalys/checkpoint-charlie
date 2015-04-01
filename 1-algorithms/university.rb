396fcce7956ea46af9c196ad9e5394c8282e2aba
stevecass <steven@mumsnet.com>
2015-04-01 09:34:07 -0400

    P1 s2 - acceptance rate

--- 1-algorithms/university.rb
+++ 1-algorithms/university.rb
@@ -7,4 +7,8 @@ class University
     self.application_stats = args['application_stats']
   end
 
+  def acceptance_rate
+    (100.0 * application_stats['accepted'] / application_stats['applied']).round
+  end
+
 end
