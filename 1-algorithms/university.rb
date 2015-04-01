006e29b7fa8ea82d400dbe07f296ddfcd0effda1
Lucian Kahn <luciankahn@gmail.com>
2015-04-01 10:11:36 -0400

    finish 1b

--- 1-algorithms/university.rb
+++ 1-algorithms/university.rb
@@ -9,31 +9,3 @@ class University
 
 end
 
-app1 = {'applied' => 170,
-       'rejected' => 90,
-       'accepted' => 70
-       }
-app2 = {'applied' => 200,
-       'rejected' => 100,
-       'accepted' => 100
-       }
-app3 = {'applied' => 180,
-       'rejected' => 20,
-       'accepted' => 160
-       }
-app4 = {'applied' => 3000,
-       'rejected' => 100,
-       'accepted' => 2900
-       }
-
-
-uni1 = University.new(name: "Yale",
-                      application_stats: app1)
-uni2 = University.new(name: "Brown",
-                      application_stats: app2)
-uni3 = University.new(name: "Hogwarts",
-                      application_stats: app3)
-uni4 = University.new(name: "Ghost",
-                      application_stats: app4)
-
-universities = [uni1, uni2, uni3, uni4]
--- 1-algorithms/university_part_1b_spec.rb
+++ 1-algorithms/university_part_1b_spec.rb
@@ -12,19 +12,45 @@ describe "Part 1b: Your Array of Universities" do
   # | Notre Dame | 500       | 220      | 230      |
   # | Toledo     | 404       | 180      | 224      |
 
-  let(:universities) {}
+  purdue_app = {'applied' => 170,
+         'rejected' => 90,
+         'accepted' => 70
+         }
+  indiana_app = {'applied' => 300,
+         'rejected' => 135,
+         'accepted' => 165
+         }
+  notre_dame_app = {'applied' => 500,
+         'rejected' => 220,
+         'accepted' => 230
+         }
+  toledo_app = {'applied' => 404,
+         'rejected' => 180,
+         'accepted' => 224
+         }
+
+  purdue = University.new({name: "Purdue",
+                        application_stats: purdue_app})
+  indiana = University.new({name: "Indiana",
+                        application_stats: indiana_app})
+  notre_dame = University.new({name: "Notre Dame",
+                        application_stats: notre_dame_app})
+  toledo = University.new({name: "Toledo",
+                        application_stats: toledo_app})
+
+  universities = [purdue, indiana, notre_dame, toledo]
 
   it 'should have a length of four' do
     expect(universities.length).to eq(4)
   end
 
   #What's a good name for this test? Give it one.
-  it 'you need to name this test! #1' do
+  it 'should contain only objects of the type University' do
     expect(universities.all?{ |university| university.class == University }).to be(true)
   end
 
   #Give a good name for this test too!
-  it 'you need to name this test! #2' do
+  it 'each stat in application stats should be greater than 0' do
     stats = universities.map(&:application_stats).map(&:values).flatten.all? { |stat| stat > 0 }
     expect(stats).to be(true)
   end
