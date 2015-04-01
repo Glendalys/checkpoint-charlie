86e92f3b9b3388bfa9fbe8c3b1563fd29312a2d1
Carl Swanson <cetswanson@gmail.com>
2015-04-01 10:50:09 -0400

    passing 8 of 12 tests

--- 2-modeling/imdb_clone.rb
+++ 2-modeling/imdb_clone.rb
@@ -3,28 +3,38 @@
 
 class Actor
 
-  def initialize(first_name, last_name, birth_name, photo_name, photo_url)
+  attr_reader :first_name, :last_name, :birth_name, :photo_name, :photo_url, :photo
 
-    @first_name = first_name
-    @last_name = last_name
-    @birth_name = birth_name
-    @photo_name = photo_name
-    @photo_url = photo_url
+  def initialize(args)
+    @first_name = args[:first_name]
+    @last_name = args[:last_name]
+    @birth_name = args[:birth_name]
+    @photo_name = args[:photo_name]
+    @photo_url = args[:photo_url]
+    @photo = [@photo_name, @photo_url]
+  end
 
+  def self.name
+    self[0]
+  end
+
+  def self.url
+    self[1]
   end
 
 end
 
 class Movie
 
-  def initialize(name, synopsis, release_year, photo_name, photo_url)
+  attr_reader :name, :synopsis, :release_year, :photo_name, :photo_url, :photo
 
-    @name = name
-    @synopsis = synopsis
-    @release_year = release_year
-    @photo_name = photo_name
-    @photo_url = photo_url
-
+  def initialize(args)
+    @name = args[:name]
+    @synopsis = args[:synopsis]
+    @release_year = args[:release_year]
+    @photo_name = args[:photo_name]
+    @photo_url = args[:photo_url]
+    @photo = {"name" => @photo_name, "url" => @photo_url}
   end
 
 end
