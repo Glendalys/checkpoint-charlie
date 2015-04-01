a287a361213f962bb4e365e7c8fddb2126948d64
stevecass <steven@mumsnet.com>
2015-04-01 09:54:58 -0400

    Part 2 - imdb clone

--- 2-modeling/imdb_clone.rb
+++ 2-modeling/imdb_clone.rb
@@ -1,2 +1,30 @@
 # Put your implementation here.
 # The tests in the spec file are your guide.
+
+class Photo
+  attr_reader :name, :url
+  def initialize args
+    @name = args[:photo_name]
+    @url  =  args[:photo_url]
+  end
+end
+
+class Actor
+  attr_reader :first_name, :last_name, :birth_name, :photo
+  def initialize args
+    @first_name = args[:first_name]
+    @last_name  = args[:last_name]
+    @birth_name = args[:birth_name]
+    @photo      = Photo.new args
+  end
+end
+
+class Movie
+  attr_reader :name, :synopsis, :release_year, :photo
+  def initialize args
+    @name         = args[:name]
+    @synopsis     = args[:synopsis]
+    @release_year = args[:release_year]
+    @photo        = Photo.new args
+  end
+end
--- 2-modeling/imdb_clone_spec.rb
+++ 2-modeling/imdb_clone_spec.rb
@@ -21,9 +21,8 @@ describe Actor do
     expect(actor.birth_name).to eq("William Bradley Pitt")
   end
 
-  pending 'should have a photo' do
-    #Write a test to show that Actor#photo returns a Photo
-    #Remember to change "pending" to "it" when you're ready to run this test
+  it 'should have a photo' do
+    expect(actor.photo).to be_a Photo
   end
 
   it "the actor's photo should have a name" do
@@ -56,9 +55,8 @@ describe Movie do
     expect(movie.release_year).to eq("1999")
   end
 
-  pending 'should have a photo' do
-    #Write a test to show that Movie#photo returns a Photo
-    #Remember to change "pending" to "it" when you're ready to run this test
+  it 'should have a photo' do
+    expect(movie.photo).to be_a Photo
   end
 
   it "the movie's photo should have a name" do
