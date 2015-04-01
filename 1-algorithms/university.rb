fd04ab7a7ee985608364083cb616ac110d67d361
Nikolai Calder <redlacialokin@gmail.com>
2015-04-01 12:02:42 -0400

    initial commit

--- 1-algorithms/university.rb
+++ 1-algorithms/university.rb
@@ -2,7 +2,27 @@ class University
 
   attr_accessor :name, :application_stats
 
-  def initialize(args)
+  def initialize(name, application_stats = {})
+      @name = name
+      @application_stats = application_stats
+      @application_stats['rejected'] = 90
+      @application_stats['accepted'] = 80
+      @application_stats['applied'] = 170
   end
 
+
+def acceptance_rate
+
+
 end
+end
+
+
+
+purdue = University.new("Purdue")
+ p purdue.name
+ p purdue.application_stats
+
+
+
+
