c54a47f175edd8d2a40b8d758dde9bec3a5d8a4e
Ben Morimoto <bkmorimoto@gmail.com>
2015-04-01 09:23:47 -0400

    finished part 3

--- 1-algorithms/university.rb
+++ 1-algorithms/university.rb
@@ -15,5 +15,12 @@ class University
     (application_stats["accepted"]/application_stats["applied"].to_f * 100).to_i
   end
 
+end
 
+def low_acceptance_count(university_array)
+  count = 0
+  university_array.each do |university|
+    count += 1 if university.acceptance_rate < 50
+  end 
+  count
 end
--- 1-algorithms/university_part_3_spec.rb
+++ 1-algorithms/university_part_3_spec.rb
@@ -6,7 +6,33 @@ describe 'Part 3: Algorithm' do
   # We're going to be using the same array of universities from "Part 1b: Your Array of Universities",
   # but you'll still need to fill in this `let` block below.
 
-  let(:universities) {}
+  purdue_args = {
+    name: "Purdue",
+    applied: 170,
+    rejected: 90,
+    accepted: 80
+  }
+  indiana_args = {
+    name: "Indiana",
+    applied: 300,
+    rejected: 135,
+    accepted: 165
+  }
+  notre_dame_args = {
+    name: "Notre Dame",
+    applied: 500,
+    rejected: 220,
+    accepted: 230
+  }
+  toledo_args = {
+    name: "Toledo",
+    applied: 404,
+    rejected: 180,
+    accepted: 224
+  }
+
+  let(:universities) {[University.new(purdue_args), University.new(indiana_args), University.new(notre_dame_args), University.new(toledo_args)]}
+
 
   it 'should find 2 universities' do
     expect(low_acceptance_count(universities)).to eq(2)
