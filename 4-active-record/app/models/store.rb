1006ec7bdb2d2308b63e0b5b3d3c90dd4e66270b
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-04-01 11:20:36 -0400

    Actual mvp for 4

--- 4-active-record/app/models/store.rb
+++ 4-active-record/app/models/store.rb
@@ -1,4 +1,4 @@
 class Store < ActiveRecord::Base
-  has_many :coats
+  # has_many :coats
   validates :name, :location, :years_open, {:presence => true}
 end
--- 4-active-record/db/migrate/20150401102607_create_coats.rb
+++ 4-active-record/db/migrate/20150401102607_create_coats.rb
@@ -5,7 +5,7 @@ class CreateCoats < ActiveRecord::Migration
       t.string :style, null: false
       t.string :size, null: false, limit: 1
       t.string :color, null: false
-      t.decimal :price, null: false, precision: 8, scale: 2
+      t.integer :price, null: false
 
       t.timestamps
     end
