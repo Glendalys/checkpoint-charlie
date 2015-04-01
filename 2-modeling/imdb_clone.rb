84265652c8994e75e5aa83372e3e38289178f99b
costolo <ben.costolo@gmail.com>
2015-04-01 10:09:57 -0400

    add solution to algorith and modeling

--- 2-modeling/imdb_clone.rb
+++ 2-modeling/imdb_clone.rb
@@ -1,2 +1,27 @@
-# Put your implementation here.
-# The tests in the spec file are your guide.
+class Photo
+	attr_accessor :name, :url
+	def initialize(name, url)
+		@name = name
+		@url = url
+	end
+end
+
+class Actor
+	attr_accessor :first_name, :last_name, :birth_name, :photo
+	def initialize(args)
+		@first_name = args[:first_name]
+		@last_name = args[:last_name]
+		@birth_name = args[:birth_name]
+		@photo = Photo.new(args[:photo_name], args[:photo_url])
+	end
+end
+
+class Movie
+	attr_accessor :name, :synopsis, :release_year, :photo
+	def initialize(args)
+		@name = args[:name]
+		@synopsis = args[:synopsis]
+		@release_year = args[:release_year]
+		@photo = Photo.new(args[:photo_name], args[:photo_url])
+	end
+end
