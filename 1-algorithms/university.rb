98d9e1b7d4906568a95f66a87e4892b549e53a74
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-04-01 11:46:51 -0400

    Final refactored solution for 1

--- 1-algorithms/university.rb
+++ 1-algorithms/university.rb
@@ -8,9 +8,16 @@ class University
   end
 
   def acceptance_rate
-    accepted = self.application_stats['accepted'].to_f
-    applied = self.application_stats['applied'].to_f
-    (accepted/applied * 100).floor
+    (application_stats['accepted'].to_f/application_stats['applied'].to_f * 100).floor
   end
 
+  def low_acceptance
+    acceptance_rate < 50 ? true : false
+  end
+
+end
+
+
+def low_acceptance_count(universities)
+  universities.partition{|university| university.low_acceptance}[0].count
 end
--- 1-algorithms/university_part_3_spec.rb
+++ 1-algorithms/university_part_3_spec.rb
@@ -6,7 +6,7 @@ describe 'Part 3: Algorithm' do
   # We're going to be using the same array of universities from "Part 1b: Your Array of Universities",
   # but you'll still need to fill in this `let` block below.
 
-  let(:universities) {}
+ let(:universities) {[University.new(name: "Purdue", application_stats: {'rejected' => 90, 'accepted' => 80, 'applied' => 170}), University.new(name: "Indiana", application_stats: {'rejected' => 135, 'accepted' => 165, 'applied' => 300}), University.new(name: "Notre Dame", application_stats: {'rejected' => 220, 'accepted' => 230, 'applied' => 500}), University.new(name: "Toledo", application_stats: {'rejected' => 180, 'accepted' => 224, 'applied' => 404})]}
 
   it 'should find 2 universities' do
     expect(low_acceptance_count(universities)).to eq(2)
