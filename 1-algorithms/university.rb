9f1e61eb099e70e6ee44ab27b8e1329dd28ba9fe
Lucian Kahn <luciankahn@gmail.com>
2015-04-01 10:30:34 -0400

    finished challenge 1

--- 1-algorithms/university.rb
+++ 1-algorithms/university.rb
@@ -7,11 +7,18 @@ class University
     @application_stats = args[:application_stats]
   end
 
-  # Write an `acceptance_rate` method that returns the percentage of accepted students at a university.
-  # This number should be an integer representing the percent. The total number of applications is the 'applied' count.
   def acceptance_rate
     @application_stats['accepted'].to_f / @application_stats['applied'].to_f * 100
   end
 
 end
 
+# Write a method `low_acceptance_count` that returns a an integer of the count of universities with an acceptance rate of less than 50.
+
+def low_acceptance_count(array_of_universities)
+  less_than_fifty = []
+  array_of_universities.each do |university|
+    less_than_fifty << university if university.acceptance_rate < 50
+  end
+  less_than_fifty.length
+end
--- 1-algorithms/university_part_3_spec.rb
+++ 1-algorithms/university_part_3_spec.rb
@@ -6,7 +6,35 @@ describe 'Part 3: Algorithm' do
   # We're going to be using the same array of universities from "Part 1b: Your Array of Universities",
   # but you'll still need to fill in this `let` block below.
 
-  let(:universities) {}
+  let(:universities) {
+    purdue_app = {'applied' => 170,
+           'rejected' => 90,
+           'accepted' => 70
+           }
+    indiana_app = {'applied' => 300,
+           'rejected' => 135,
+           'accepted' => 165
+           }
+    notre_dame_app = {'applied' => 500,
+           'rejected' => 220,
+           'accepted' => 230
+           }
+    toledo_app = {'applied' => 404,
+           'rejected' => 180,
+           'accepted' => 224
+           }
+
+    purdue = University.new({name: "Purdue",
+                          application_stats: purdue_app})
+    indiana = University.new({name: "Indiana",
+                          application_stats: indiana_app})
+    notre_dame = University.new({name: "Notre Dame",
+                          application_stats: notre_dame_app})
+    toledo = University.new({name: "Toledo",
+                          application_stats: toledo_app})
+
+    universities = [purdue, indiana, notre_dame, toledo]
+  }
 
   it 'should find 2 universities' do
     expect(low_acceptance_count(universities)).to eq(2)
