e31cab91f90a96ef50143b8df514f1ecf7ece5dd
Ben Morimoto <bkmorimoto@gmail.com>
2015-04-01 11:03:51 -0400

    fixed types in 4/ar-queries

--- 4-active-record/app/models/coat.rb
+++ 4-active-record/app/models/coat.rb
@@ -4,5 +4,4 @@ class Coat < ActiveRecord::Base
   validates_presence_of :store
   validates :brand, :color, :length, {:presence => true}
   validates :length, {:numericality => {greater_than_or_equal_to: 1}}
-  # Remember to create a migration!
 end
--- 4-active-record/app/models/store.rb
+++ 4-active-record/app/models/store.rb
@@ -1,6 +1,6 @@
 class Store < ActiveRecord::Base
   has_many :coats
-  # Remember to create a migration!
+
   validates :name, :location, :years_since_opened, {:presence => true}
   validates :years_since_opened, {:numericality => true}
 end
